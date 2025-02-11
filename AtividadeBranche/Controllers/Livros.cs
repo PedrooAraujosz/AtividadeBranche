622c528f70f19ffe77fddb8d60dc2f9dd52782ba
Ana Geremias <anageremiias@gmail.com>
2025-02-11 16:38:15 -0300

    Projeto teste pusch

--- AtividadeBranche/Controllers/Livros.cs
+++ AtividadeBranche/Controllers/Livros.cs
@@ -6,5 +6,11 @@
         public string Autor { get; set; }
         public string Editora { get; set; }
 
+        public Livros(string nome, string autor, string editora)
+        {
+            Nome = nome;
+            Autor = autor;
+            Editora = editora;
+        }
     }
 }
