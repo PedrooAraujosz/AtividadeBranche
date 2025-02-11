a93571465a3d5bfffaa6f7a595800099677f4a71
Ana Geremias <anageremiias@gmail.com>
2025-02-11 16:32:27 -0300

    Projeto Mesclar

---
+++ AtividadeBranche/Controllers/Livros.cs
@@ -0,0 +1,10 @@
+﻿namespace AtividadeBranche.Controllers
+{
+    public class Livros
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+
+    }
+}
